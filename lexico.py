9e55905970077f0274c5cc210fca6a163c6219b5
Huangnli <50835276+Huangnli@users.noreply.github.com>
2020-09-22 19:37:28 -0400

    Add files via upload

---
+++ lexico.py
@@ -0,0 +1,27 @@
+# calclex.py
+from sly import Lexer
+
+class CalcLexer(Lexer):
+    # Set of token names. This is always required 
+    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
+               DIVIDE, ASSIGN, LPAREN, RPAREN }
+    
+    # String containing ignored characters between tokens
+    ignore = ' \t'
+    
+    # Regular expression rules for tokens
+    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    NUMBER  = r'\d+'
+    PLUS    = r'\+'
+    MINUS   = r'-'
+    TIMES   = r'\*'
+    DIVIDE  = r'/'
+    ASSIGN  = r'='
+    LPAREN  = r'\('
+    RPAREN  = r'\)'
+
+if __name__ == '__main__':
+    data = input('lex > ') 
+    lexer = CalcLexer()
+    for tok in lexer.tokenize(data):
+        print('type=%r, value=%r lineno=%r index=%r' % (tok.type, tok.value, tok.lineno, tok.index))
