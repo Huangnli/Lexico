e3b328cc4821224e4d205ddd39b11720229dc2a6
Huangnli <huang@DESKTOP-96V9H3L.localdomain>
2020-09-23 19:43:45 -0400

    add new tokens

--- lexico.py
+++ lexico.py
@@ -4,27 +4,57 @@ import sys
 
 class CalcLexer(Lexer):
     # Set of token names. This is always required 
-    tokens = { ID, NUMBER, PLUS, MINUS, TIMES,
-               DIVIDE, ASSIGN, LPAREN, RPAREN }
+    tokens = { ID, NUM, PLUS, MINUS, TIMES, DIVIDE, EQUAL, 
+               LPAREN, RPAREN, LBRACK, RBRACK, LBRACE, RBRACE, 
+               SCOLON, COLON, AND, LESS, DOT, NOT, KEYWORD}
     
     # String containing ignored characters between tokens
     ignore = ' \t'
-    ignore_comment = r'\/\* .* \*\\'
+    ignore_comment = r'//.*'
+    ignore_comment2 = r'/\*(.|\n)*\*/'
     
     # Regular expression rules for tokens
-    # Átomos que representam palavras reservdadas da linguagem
-
     # Átomos que representam símbolos da linguagem
     PLUS    = r'\+'
     MINUS   = r'-'
     TIMES   = r'\*'
     DIVIDE  = r'/'
-    ASSIGN  = r'='
+    EQUAL   = r'='
     LPAREN  = r'\('
     RPAREN  = r'\)'
-    # Átomos com regras de formação complexa
-    ID      = r'[a-zA-Z_][a-zA-Z0-9_]*'
-    NUMBER  = r'\d+'
+    LBRACK  = r'\[' 
+    RBRACK  = r'\]'
+    LBRACE  = r'{'
+    RBRACE  = r'}'
+    SCOLON  = r';'
+    COLON   = r','
+    AND     = r'&&'
+    LESS    = r'<'
+    DOT     = r'\.'
+    NOT     = r'!'
+
+    # Átomos com regras de formação complexa e keywords
+    ID = r'[a-zA-Z_][a-zA-Z0-9_]*'
+    ID['class']              = KEYWORD
+    ID['String']             = KEYWORD
+    ID['public']             = KEYWORD
+    ID['static']             = KEYWORD
+    ID['void']               = KEYWORD
+    ID['main']               = KEYWORD
+    ID['extends']            = KEYWORD
+    ID['return']             = KEYWORD
+    ID['boolean']            = KEYWORD
+    ID['int']                = KEYWORD
+    ID['if']                 = KEYWORD
+    ID['else']               = KEYWORD
+    ID['while']              = KEYWORD
+    ID['System.out.println'] = KEYWORD
+    ID['true']               = KEYWORD
+    ID['false']              = KEYWORD
+    ID['this']               = KEYWORD
+    ID['new']                = KEYWORD
+    ID['length']             = KEYWORD
+    NUM = r'\d+'
 
     def error (self, t):
         print("Linha: %d - Caractere ilegal: %s" % (t.lineno, t.value))
@@ -43,4 +73,3 @@ if __name__ == '__main__':
                 print('[ %r, %r, %r ]' % (tok.lineno, tok.type, tok.value))
         except:
             print("Arquivo nao existe!")
-            
